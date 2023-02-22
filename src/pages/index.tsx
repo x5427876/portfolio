af39e117ffbb9730b0f710f1879d7a386a3bf186
Gavin Gong <gavingong@ip-10-11-81-22.ap-northeast-1.compute.internal>
2023-02-22 11:18:56 +0800

    new favicon and page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Head from "next/head";
 
 import InstagramIcon from "../../images/instagram.svg";
 import GithubIcon from "../../images/github.svg";
@@ -7,45 +8,53 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="w-[70%] h-[calc(100vh-80px)] py-12` flex flex-col-reverse md:flex-row justify-between text-white">
-            <div className="w-full h-[45%] md:w-[60%] md:h-full flex flex-col justify-center">
-                <div className="text-3xl md:text-5xl font-bold">
-                    <span className="bg-gray-500">
-                        Hi, My Name Is Gavin Kong.
-                    </span>
+        <>
+            <Head>
+                <title>Gavin | Home</title>
+            </Head>
+            <div className="w-[70%] h-[calc(100vh-80px)] py-12` flex flex-col-reverse md:flex-row justify-between text-white">
+                <div className="w-full h-[45%] md:w-[60%] md:h-full flex flex-col justify-center">
+                    <div className="text-3xl md:text-5xl font-bold">
+                        <span className="bg-gray-500">
+                            Hi, My Name Is Gavin Kong.
+                        </span>
+                    </div>
+                    <p className="mt-8 text-xl md:text-2xl">
+                        Frontend developer, Rock music lover, and Gamer based in
+                        Taipei, Taiwan.
+                    </p>
+                    <div className="mt-12 flex">
+                        <Link
+                            href="https://www.instagram.com/kongtse.0613/"
+                            target="_blank"
+                        >
+                            <InstagramIcon className="w-[32px] h-[32px] mr-4 fill-white" />
+                        </Link>
+                        <Link
+                            href="https://github.com/x5427876"
+                            target="_blank"
+                        >
+                            <GithubIcon className="w-[32px] h-[32px] mr-4 fill-white" />
+                        </Link>
+                        <Link
+                            href="https://www.linkedin.com/in/gavinkong/"
+                            target="_blank"
+                        >
+                            <LinkedIcon className="w-[32px] h-[32px] mr-4 fill-white" />
+                        </Link>
+                    </div>
                 </div>
-                <p className="mt-8 text-xl md:text-2xl">
-                    Frontend developer, Rock music lover, and Gamer based in
-                    Taipei, Taiwan.
-                </p>
-                <div className="mt-12 flex">
-                    <Link
-                        href="https://www.instagram.com/kongtse.0613/"
-                        target="_blank"
-                    >
-                        <InstagramIcon className="w-[32px] h-[32px] mr-4 fill-white" />
-                    </Link>
-                    <Link href="https://github.com/x5427876" target="_blank">
-                        <GithubIcon className="w-[32px] h-[32px] mr-4 fill-white" />
-                    </Link>
-                    <Link
-                        href="https://www.linkedin.com/in/gavinkong/"
-                        target="_blank"
-                    >
-                        <LinkedIcon className="w-[32px] h-[32px] mr-4 fill-white" />
-                    </Link>
+                <div className="w-full h-[45%] md:w-[30%] md:h-full flex items-center justify-center">
+                    <Image
+                        priority
+                        className="rounded-full w-auto md:w-full md:h-auto aspect-square"
+                        src="/avatar.jpeg"
+                        alt="Picture of the author"
+                        width={500}
+                        height={500}
+                    />
                 </div>
             </div>
-            <div className="w-full h-[45%] md:w-[30%] md:h-full flex items-center justify-center">
-                <Image
-                    priority
-                    className="rounded-full w-auto md:w-full md:h-auto aspect-square"
-                    src="/avatar.jpeg"
-                    alt="Picture of the author"
-                    width={500}
-                    height={500}
-                />
-            </div>
-        </div>
+        </>
     );
 }
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,46 +1,54 @@
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 import { roboto } from "../_app";
 import LinkIcon from "../../../images/link.svg";
 
 const Projects = () => {
     return (
-        <div className="w-[80%] text-white py-12">
-            <h1 className="text-3xl md:text-5xl font-bold mb-12">Projects</h1>
-            <div className={`${roboto.className}`}>
-                <div className="rounded-lg border-2 border-gray-500 p-5 flex flex-col md:flex-row">
-                    <div className="md:w-1/2">
-                        <h1 className="text-3xl">Spotify Profile</h1>
-                        <div className="py-6 md:mr-8 leading-relaxed">
-                            A web application for visualizing personalized
-                            Spotify data. View your top artists, top tracks,
-                            recently played tracks, you can click to listen to
-                            songs directly.
+        <>
+         <Head>
+                <title>Gavin | Projects</title>
+            </Head>
+            <div className="w-[80%] text-white py-12">
+                <h1 className="text-3xl md:text-5xl font-bold mb-12">
+                    Projects
+                </h1>
+                <div className={`${roboto.className}`}>
+                    <div className="rounded-lg border-2 border-gray-500 p-5 flex flex-col md:flex-row">
+                        <div className="md:w-1/2">
+                            <h1 className="text-3xl">Spotify Profile</h1>
+                            <div className="py-6 md:mr-8 leading-relaxed">
+                                A web application for visualizing personalized
+                                Spotify data. View your top artists, top tracks,
+                                recently played tracks, you can click to listen
+                                to songs directly.
+                            </div>
+                            <Link
+                                className="flex items-center justify-center cursor-pointer bg-white p-2 rounded-lg w-1/3 mb-6"
+                                href="https://github.com/x5427876/spotify-profile"
+                                target="_blank"
+                            >
+                                <p className="mr-2 text-black">Github</p>
+                                <LinkIcon className="fill-black w-[12px] h-[12px]" />
+                            </Link>
                         </div>
-                        <Link
-                            className="flex items-center justify-center cursor-pointer bg-white p-2 rounded-lg w-1/3 mb-6"
-                            href="https://github.com/x5427876/spotify-profile"
-                            target="_blank"
-                        >
-                            <p className="mr-2 text-black">Github</p>
-                            <LinkIcon className="fill-black w-[12px] h-[12px]" />
-                        </Link>
-                    </div>
-                    <div className="md:w-1/2 flex items-center justify-center">
-                        <Image
-                            priority
-                            className="rounded-lg"
-                            src="/spotify-profile-preview.png"
-                            alt="spotify-profile-preview"
-                            width={500}
-                            height={500}
-                        />
+                        <div className="md:w-1/2 flex items-center justify-center">
+                            <Image
+                                priority
+                                className="rounded-lg"
+                                src="/spotify-profile-preview.png"
+                                alt="spotify-profile-preview"
+                                width={500}
+                                height={500}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
